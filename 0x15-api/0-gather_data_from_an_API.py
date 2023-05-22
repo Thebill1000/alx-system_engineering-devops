e0c6766015a960412d2960bee22951d95c909c92
thebill1000 <williamsbilljnr100@gmail.com>
2023-05-22 15:11:18 -0700

    APIS

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -3,37 +3,18 @@
 Python script that, using this REST API, for a given employee ID,
 returns information about his/her TODO list progress
 """
-import requests
+if __name__ == "__main__":
+    import requests
+    from sys import argv
 
-def get_employee_todo_progress(employee_id):
-    # Send a GET request to the API endpoint
-    response = requests.get(f'https://jsonplaceholder.typicode.com/users/{employee_id}/todos')
+    url = 'https://jsonplaceholder.typicode.com/users/{}'.format(argv[1])
+    employee = requests.get(url).json().get('name')
 
-    # Check if the request was successful (status code 200)
-    if response.status_code == 200:
-        todos = response.json()
-
-        # Get the employee's name
-        employee_name = todos[0]['name'].split()[0]
-
-        # Count the number of completed tasks
-        completed_tasks = [todo for todo in todos if todo['completed']]
-        number_of_done_tasks = len(completed_tasks)
-
-        # Calculate the total number of tasks
-        total_number_of_tasks = len(todos)
-
-        # Display the progress information
-        print(f"Employee {employee_name} is done with tasks ({number_of_done_tasks}/{total_number_of_tasks}):")
-
-        # Display the titles of completed tasks
-        for task in completed_tasks:
-            print("\t", task['title'])
-    else:
-        print(f"Error: Unable to retrieve TODO list for employee {employee_id}.")
-
-# Prompt the user to enter an employee ID
-employee_id = int(input("Enter the employee ID: "))
-
-# Call the function to get the employee's TODO list progress
-get_employee_todo_progress(employee_id)
+    url_todo = 'https://jsonplaceholder.typicode.com/todos?userId={}'.format(
+        argv[1])
+    todos = requests.get(url_todo).json()
+    done = [todo for todo in todos if todo.get('completed')]
+    print("Employee {} is done with tasks({}/{}):".format(
+        employee, len(done), len(todos)))
+    for d in done:
+        print("\t ", d.get('title'))
