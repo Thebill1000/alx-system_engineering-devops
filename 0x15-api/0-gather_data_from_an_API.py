c00b9b281dc0227f72a91ee31f28d88cfadba51c
thebill1000 <williamsbilljnr100@gmail.com>
2023-05-22 15:00:25 -0700

    APIS

---
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""Returns to-do list information for a given employee ID."""
+import requests
+import sys
+
+if __name__ == "__main__":
+    url = "https://jsonplaceholder.typicode.com/"
+    user = requests.get(url + "users/{}".format(sys.argv[1])).json()
+    todos = requests.get(url + "todos", params={"userId": sys.argv[1]}).json
---
+++ 0x15-api/README.md
@@ -0,0 +1,1 @@
+Join me as I learn to create APis
